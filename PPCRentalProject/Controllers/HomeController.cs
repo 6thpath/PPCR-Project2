434fd7e0a0b394640a8a8025087028e98925087c
Phát <phatmai2@vanlanguni.vn>
2017-11-19 22:28:47 +0700

    Add project files.

---
+++ PPCRentalProject/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PPCRentalProject.Models;
+
+namespace PPCRentalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        DemoPPCRentalEntities entities = new DemoPPCRentalEntities();
+        public ActionResult Index(int? page)
+        {
+            var props = entities.PROPERTies.ToList();
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+            return View(props.ToPagedList(pageNumber, pageSize));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
