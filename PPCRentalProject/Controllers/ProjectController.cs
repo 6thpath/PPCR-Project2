20303a2ea9e2ed34b369d2f274ad39434f84ee7f
Phát <phatmai2@vanlanguni.vn>
2017-11-21 15:07:13 +0700

    Update Layout, add View Projects List and Controller

---
+++ PPCRentalProject/Controllers/ProjectController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCRentalProject.Models;
+using PagedList;
+using PPCRentalProject.ViewModels;
+
+
+namespace PPCRentalProject.Controllers
+{
+    public class ProjectController : Controller
+    {
+        DemoPPCRentalEntities entities = new DemoPPCRentalEntities();
+        // GET: Project
+        public ActionResult User_ViewProjectList(int? page)
+        {
+            ViewModel Vm = new ViewModel();
+            Vm.zDistricts = entities.DISTRICTs.ToList();
+            Vm.zWards = entities.WARDs.ToList();
+            Vm.zStreets = entities.STREETs.ToList();
+            Vm.zProperties = entities.PROPERTies.ToList();
+            //var Vm = entities.PROPERTies.ToList();
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(Vm.zProperties.ToPagedList(pageNumber, pageSize));
+        }
+    }
+}
---
+++ PPCRentalProject/ViewModels/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PPCRentalProject.Models;
+using PagedList;
+
+namespace PPCRentalProject.ViewModels
+{
+    public class ViewModel
+    {
+        public List<DISTRICT> zDistricts { get; set; }
+        public List<PROPERTY> zProperties { get; set; }
+        public List<STREET> zStreets { get; set; }
+        public List<WARD> zWards { get; set; }
+    }
+}
