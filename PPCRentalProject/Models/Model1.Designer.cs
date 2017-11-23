4dcf984c832740876b8a1c95085698991bf9fdcb
Phát <tienphat1501@gmail.com>
2017-11-23 18:57:02 +0700

    Small update

--- PPCRentalProject/Models/Model1.Designer.cs
+++ PPCRentalProject/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Vinh\Source\Repos\PPCRental\PPCRentalProject\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\lPaths\source\repos\PPCRentalProject\PPCRentalProject\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
